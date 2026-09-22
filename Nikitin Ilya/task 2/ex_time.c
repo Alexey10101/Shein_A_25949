7461f3c265b106d58af2f9d461089103474519f4
WeKnowNoFear <badrov19991@gmail.com>
2026-09-22 18:55:12 +0700

    add task 2

--- Nikitin Ilya/task 2/ex_time.c
+++ Nikitin Ilya/task 2/ex_time.c
@@ -17,12 +17,13 @@ int main(void)
     time(&now);
     sp = localtime(&now);
 
-    printf("%02d/%02d/%04d %02d:%02d %s\n",
+    printf("%02d/%02d/%04d %02d:%02d:%02d %s\n",
            sp->tm_mon + 1,
            sp->tm_mday,
            sp->tm_year + 1900,
            sp->tm_hour,
            sp->tm_min,
+           sp->tm_sec,
            tzname[sp->tm_isdst]);
 
     return 0;
